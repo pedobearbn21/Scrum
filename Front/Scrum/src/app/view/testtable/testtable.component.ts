fea0faa19f03172f64f4db7017427fb3e4467366
pedobearbn21 <39798687+pedobearbn21@users.noreply.github.com>
2019-06-28 22:09:47 +0700

    14

---
+++ Front/Scrum/src/app/view/testtable/testtable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testtable',
+  templateUrl: './testtable.component.html',
+  styleUrls: ['./testtable.component.scss']
+})
+export class TesttableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
